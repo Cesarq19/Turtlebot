f413358d81439a4906582c69fa9b0868ee11b6d7
cesar <Cesarq19>
2023-09-01 14:56:03 -0500

    Actualizacion 85

---
+++ turtlebot3_mobility/include/turtlebot3_mobility/velocity_cmd_node.hpp
@@ -0,0 +1,30 @@
+#ifndef VELOCITY_CMD_NODE_HPP_
+#define VELOCITY_CMD_NODE_HPP_
+
+#include <array>
+#include <chrono>
+#include <list>
+#include <map>
+#include <memory>
+#include <mutex>
+#include <string>
+#include <queue>
+
+#include <geometry_msgs/msg/twist.hpp>
+#include <tf2_ros/transform_broadcaster.h>
+#include "rclcpp/rclcpp.hpp"
+#include "rcutils/cmdline_parser.h"
+#include "dynamixel_sdk/dynamixel_sdk.h"
+
+class VelocityCmdNode : public rclcpp::Node
+{
+    public:
+        VelocityCmdNode();
+        virtual ~VelocityCmdNode();
+
+    private:
+        void cmd_vel_callback();
+        rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_sub_;
+
+
+}
