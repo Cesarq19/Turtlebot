49f7ee5d8871617eacb0526b9db00c2ba40f2153
Cesar Quintuña <90804290+Cesarq19@users.noreply.github.com>
2023-09-21 14:09:21 -0500

    Update ydlidar_ros2_driver_node.cpp

--- ydlidar_ros2_driver/src/ydlidar_ros2_driver_node.cpp
+++ ydlidar_ros2_driver/src/ydlidar_ros2_driver_node.cpp
@@ -42,8 +42,8 @@ int main(int argc, char *argv[]) {
 
   CYdLidar laser;
   std::string str_optvalue;
-  node->declare_parameter<std::string>("port", "/dev/ttyUSB0");
-  node->get_parameter_or<std::string>("port", str_optvalue, "/dev/ttyUSB0");
+  node->declare_parameter<std::string>("port", "/dev/ttyUSB1");
+  node->get_parameter_or<std::string>("port", str_optvalue, "/dev/ttyUSB1");
   ///lidar port
   laser.setlidaropt(LidarPropSerialPort, str_optvalue.c_str(), str_optvalue.size());
 
