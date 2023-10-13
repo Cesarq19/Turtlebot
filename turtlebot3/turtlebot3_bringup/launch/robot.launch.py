e2efd4c9f2bbd57644757f58545b1ba5bb6b4d28
Cesarq19 <cepaquin@espol.edu.ec>
2023-10-13 14:25:09 -0500

    Puerto LIdar sdk

--- turtlebot3/turtlebot3_bringup/launch/robot.launch.py
+++ turtlebot3/turtlebot3_bringup/launch/robot.launch.py
@@ -31,7 +31,7 @@ from launch_ros.actions import Node
 def generate_launch_description():
     TURTLEBOT3_MODEL = os.environ['TURTLEBOT3_MODEL']
     LDS_MODEL = os.environ['LDS_MODEL']
-    LDS_LAUNCH_FILE = '/hlds_laser.launch.py'
+    LDS_LAUNCH_FILE = '/ydlidar_launch.py'
     IMU_LAUNCH_FILE = '/wt61c.launch.py'
 
     usb_port = LaunchConfiguration('usb_port', default='/dev/serial/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.4:1.0-port0')
@@ -44,15 +44,15 @@ def generate_launch_description():
             TURTLEBOT3_MODEL + '.yaml'))
 
     # Inicializar el lidar
+    lidar_pkg_dir = LaunchConfiguration(
+        'lidar_pkg_dir',
+       default=os.path.join(get_package_share_directory('ydlidar_ros2_driver'), 'launch')
+    )
+
     #lidar_pkg_dir = LaunchConfiguration(
     #    'lidar_pkg_dir',
     #    default=os.path.join(get_package_share_directory('ydlidar_ros2_driver'), 'launch')
     #);
-
-    lidar_pkg_dir = LaunchConfiguration(
-        'lidar_pkg_dir',
-        default=os.path.join(get_package_share_directory('hls_lfcd_lds_driver'), 'launch')
-    );
 
     # Inicializar el IMU
     imu_pkg_dir = LaunchConfiguration(
