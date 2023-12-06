c766d4c044147a77972e255cb9dc561f13fbe055
Cesarq19 <cepaquin@espol.edu.ec>
2023-12-06 07:19:08 -0500

    lectura de cmd_vel y escritura de posicion

--- node_turtlebot/include/node_turtlebot/motores.hpp
+++ node_turtlebot/include/node_turtlebot/motores.hpp
@@ -4,6 +4,22 @@
 #include <rclcpp/rclcpp.hpp>
 #include <std_msgs/msg/float64.hpp>
 #include <geometry_msgs/msg/twist.hpp>
+#include "dynamixel_sdk/dynamixel_sdk.h"
+
+// Control table address
+#define ADDR_OPERATING_MODE 11
+#define ADDR_TORQUE_ENABLE 64
+#define ADDR_GOAL_VELOCITY 104
+#define ADDR_PRESENT_VELOCITY 128
+
+// Protocol version
+#define PROTOCOL_VERSION 2.0 
+
+// Default setting
+#define MOTOR_LEFT_ID 1
+#define MOTOR_RIGHT_ID 0
+#define BAUDRATE 57600                                                                                // Default Baudrate of DYNAMIXEL X series
+#define DEVICE_NAME "/dev/serial/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.4:1.0-port0" // [Linux]: "/dev/ttyUSB*", [Windows]: "COM*"
 
 class MotoresNode : public rclcpp::Node {
 public:
@@ -12,8 +28,8 @@ public:
 private:
     void cmdVelCallback(const geometry_msgs::msg::Twist::SharedPtr msg);
     
-    rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr left_motor_publisher_;
-    rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr right_motor_publisher_;
+    rclcpp::Publisher<std_msgs::msg::Int32>::SharedPtr left_motor_publisher_;
+    rclcpp::Publisher<std_msgs::msg::Int32>::SharedPtr right_motor_publisher_;
     rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_subscription_;
 };
 
--- node_turtlebot/src/motores.cpp
+++ node_turtlebot/src/motores.cpp
@@ -1,5 +1,12 @@
 #include "motores.hpp"
 
+PortHandler *portHandler;
+PacketHandler *packetHandler;
+
+uint8_t dxl_error = 0;
+int dxl_comm_result = COMM_TX_FAIL;
+uint32_t goal_velocity = 0;
+
 MotoresNode::MotoresNode() : Node("motores_node") {
     // Publicadores para los motores izquierdo y derecho
     left_motor_publisher_ = this->create_publisher<std_msgs::msg::Float64>("left_motor_cmd", 10);
@@ -24,9 +31,88 @@ void MotoresNode::cmdVelCallback(const geometry_msgs::msg::Twist::SharedPtr msg)
     right_motor_publisher_->publish(right_motor_cmd);
 }
 
+void setupMotors()
+{
+    // Enable torque left motor
+    packetHandler->write1ByteTxRx(
+        portHandler,
+        MOTOR_LEFT_ID,
+        ADDR_TORQUE_ENABLE,
+        1,
+        &dxl_error);
+    // Enable torque right motor
+    packetHandler->write1ByteTxRx(
+        portHandler,
+        MOTOR_RIGHT_ID,
+        ADDR_TORQUE_ENABLE,
+        1,
+        &dxl_error);
+    // Use Velocity Control Mode
+    dxl_comm_result = packetHandler->write1ByteTxRx(
+        portHandler,
+        MOTOR_LEFT_ID,
+        ADDR_OPERATING_MODE,
+        1,
+        &dxl_error);
+
+    if (dxl_comm_result != COMM_SUCCESS)
+    {
+        RCLCPP_ERROR(rclcpp::get_logger("motores_node"), "Failed to set Velocity Control Mode.");
+    }
+    else
+    {
+        RCLCPP_INFO(rclcpp::get_logger("motores_node"), "Succeeded to set Velocity Control Mode.");
+    }
+
+    // Use Velocity Control Mode
+    dxl_comm_result = packetHandler->write1ByteTxRx(
+        portHandler,
+        MOTOR_RIGHT_ID,
+        ADDR_OPERATING_MODE,
+        1,
+        &dxl_error);
+
+    if (dxl_comm_result != COMM_SUCCESS)
+    {
+        RCLCPP_ERROR(rclcpp::get_logger("motores_node"), "Failed to set Velocity Control Mode.");
+    }
+    else
+    {
+        RCLCPP_INFO(rclcpp::get_logger("motores_node"), "Succeeded to set Velocity Control Mode.");
+    }
+}
+
 int main(int argc, char** argv) {
+    portHandler = PortHandler::getPortHandler(DEVICE_NAME);
+    packetHandler = PacketHandler::getPacketHandler(PROTOCOL_VERSION);
+
+    // Open Serial Port
+    dxl_comm_result = portHandler->openPort();
+    if (dxl_comm_result == false)
+    {
+        RCLCPP_ERROR(rclcpp::get_logger("velocity_cmd_node"), "Failed to open the port!");
+        return -1;
+    }
+    else
+    {
+        RCLCPP_INFO(rclcpp::get_logger("velocity_cmd_node"), "Succeeded to open the port.");
+    }
+
+    // Set the baudrate of the serial port (use DYNAMIXEL Baudrate)
+    dxl_comm_result = portHandler->setBaudRate(BAUDRATE);
+    if (dxl_comm_result == false)
+    {
+        RCLCPP_ERROR(rclcpp::get_logger("velocity_cmd_node"), "Failed to set the baudrate!");
+        return -1;
+    }
+    else
+    {
+        RCLCPP_INFO(rclcpp::get_logger("velocity_cmd_node"), "Succeeded to set the baudrate.");
+    }
+
     rclcpp::init(argc, argv);
     rclcpp::spin(std::make_shared<MotoresNode>());
     rclcpp::shutdown();
+    
     return 0;
 }
